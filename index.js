b4c262325a8eede7a30629bae802b1c9e5c06f6e
Lady Blue <bluelady.2015@yahoo.com>
2015-09-09 19:05:23 -0700

    adding index.js and package.json files

---
+++ index.js
@@ -0,0 +1,25 @@
+var net = require('net');
+var fs = require('fs');
+var uuid = require('uuid');
+
+var server = net.createServer(function(socket) {
+  var incomingData = "";
+  socket.on('data', function(data) {
+    console.log(data.toString());
+
+    var dataString = data.toString();
+    if (data) {
+      incomingData += dataString;
+    }
+  });
+
+  socket.on('end', function() {
+    console.log('disconnected');
+
+    var createdFile = fs.createWriteStream(uuid.v1() + '.txt');
+    createdFile.write(incomingData);
+    createdFile.end();
+  });
+});
+
+server.listen(3000);
